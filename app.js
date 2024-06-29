eae3b321c7cf82f186abe9b11c09e70244617907
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-29 12:47:44 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-// write code here
+function squareArr(arr) {
+  let sqrtArr = arr.map(num => num * num)
+  console.log(sqrtArr)
+}
+const arr = [2, 3, 5, 7, 11];
+squareArr(arr); // [4, 9, 25, 49, 121]
